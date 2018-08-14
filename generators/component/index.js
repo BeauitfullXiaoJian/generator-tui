c5d5175faccc33d74c8a04f8641642246ecd9920
BeauitfullXiaoJian <xiaojiannangkang@126.com>
2018-08-14 09:29:09 +0800

    add component cmd

---
+++ generators/component/index.js
@@ -0,0 +1,45 @@
+var Generator = require('yeoman-generator/lib');
+
+module.exports = class extends Generator {
+
+    create() {
+        return this.prompt([{
+            type: 'input',
+            name: 'moduleName',
+            message: '组件属于哪个模块？',
+        }, {
+            type: 'input',
+            name: 'pageName',
+            message: '要创建的组件名称:'
+        }]).then((answers) => {
+            this._createPage(answers.moduleName, answers.pageName);
+        });
+    }
+    _createPage(moduleName, pageName) {
+        const upName = this._getUpName(moduleName);
+        const upPageName = this._getUpName(pageName);
+        const params = { moduleName, upName, date: new Date().toLocaleString(), pageName, upPageName };
+        this.fs.copyTpl(
+            this.templatePath('p.txt'),
+            this.destinationPath(`src/app/modules/${moduleName}/components/${pageName}.component.ts`),
+            params
+        );
+        // this.fs.copyTpl(
+        //     this.templatePath('p.scss'),
+        //     this.destinationPath(`src/app/modules/${moduleName}/pages/${pageName}/${pageName}.component.scss`),
+        //     params
+        // );
+        // this.fs.copyTpl(
+        //     this.templatePath('p.html'),
+        //     this.destinationPath(`src/app/modules/${moduleName}/pages/${pageName}/${pageName}.component.html`),
+        //     params
+        // );
+    }
+    _getUpName(name) {
+        let upName = name.replace(/-(\w)/g, function (all, letter) {
+            return letter.toUpperCase();
+        });
+        upName = upName.replace(/^\S/, function (s) { return s.toUpperCase(); });
+        return upName;
+    }
+};
